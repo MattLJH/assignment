5dff1b44f8ee1ba694b53c19629f0eda59e35197
MattLJH <103894261+MattLJH@users.noreply.github.com>
2023-01-09 10:12:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MotionLayout motionLayout = findViewById(R.id.motionLayout);
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+                Intent i = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
+
+            }
+        });
+    }
+}
