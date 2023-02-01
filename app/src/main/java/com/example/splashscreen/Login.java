cf88e2b4f78e05d774a043a93d50cea53ebd260c
Haruwin <narumi.haruwin@gmail.com>
2023-02-01 20:31:40 +0800

    Improvised login page

--- app/src/main/java/com/example/splashscreen/Login.java
+++ app/src/main/java/com/example/splashscreen/Login.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
     private EditText LoginName, LoginPassword;
     private Button LoginButton;
+    private TextView RegiButton;
     private UserInfo UserDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,14 @@ public class Login extends AppCompatActivity {
         LoginName = (EditText) findViewById(R.id.LogName);
         LoginPassword = (EditText) findViewById(R.id.LogPass);
         LoginButton = (Button) findViewById(R.id.Logbutton);
+        RegiButton = (TextView) findViewById(R.id.RegiClick);
         UserDb = new UserInfo(this);
+        RegiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
